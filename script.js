fe3ab65217e510e8f667a18b38d34dec202e0654
Efe Ozturkoglu <Efe Ozturkoglu>
2016-08-27 01:26:25 -0400

    Added Javascript comments

--- script.js
+++ script.js
@@ -100,6 +100,24 @@ document.addEventListener("mousemove", function(e) {
 	mouse.y = e.pageY;
 	createParticle(e.pageX, e.pageY);
 });
+document.addEventListener("mousedown", function(e) {
+	for (var i = 0; i < 40; i++) {
+		var x = e.pageX + (Math.random() * 40) - 23;
+		var y = e.pageY + (Math.random() * 40) - 23;
+		var ang = Math.round(Math.random() * 360);
+		var i2 = Math.round(Math.random() * trailParticles.length);
+		particleWomb(x, y, i2, ang);
+	}
+});
+document.addEventListener("mouseup", function(e) {
+	for (var i = 0; i < 40; i++) {
+		var x = e.pageX + (Math.random() * 40) - 23;
+		var y = e.pageY + (Math.random() * 40) - 23;
+		var ang = Math.round(Math.random() * 360);
+		var i2 = Math.round(Math.random() * trailParticles.length);
+		particleWomb(x, y, i2, ang);
+	}
+});
 function updateMouseTrail() { // Runs every few frames
 	if (Math.random() < 0.3) createParticle(mouse.x, mouse.y);
 	for (var i = 0; i < trailParticles.length; i++) {
@@ -111,37 +129,39 @@ function updateParticle(elm) { // Helper function for updateMouseTrail()
 	elm.style.opacity = 1 - (life / trailConfig.particleLife);
 	var x = parseInt(elm.style.left),
 		y = parseInt(elm.style.top);
-	x += Math.round(Math.random() * trailConfig.particleSpeed);
-	y += Math.round(Math.random() * trailConfig.particleSpeed);
+	x += Math.round(Math.random() * elm.x);
+	y += Math.round(Math.random() * elm.y);
 	elm.style.left = x + "px";
 	elm.style.top = y + "px";
 }
 function createParticle(x, y) { // Actually just resets existing particles for performance
 	x++;
 	y++;
-	var created = false;
 	var oldest = 0;
 	for (var i = 0; i < trailParticles.length; i++) {
 		var life = Date.now() - trailParticles[i].lastUsed;
 		if (life > Date.now() - trailParticles[oldest].lastUsed) oldest = i;
 	}
-			particleWomb(x, y, oldest);
-			created = true;
-	if (!created) {
-		var rand = Math.floor(Math.random() * trailParticles.length);
-		particleWomb(x, y, rand);
-	}
+	particleWomb(x, y, oldest, trailConfig.particleAngle());
 }
 particleColors = ["#f00", "#0f0", "#00f", "#ff0", "#f0f", "#0ff", "#f80", "#f08", "#8f0", "#0f8", "#80f", "#08f"];
-function particleWomb(x, y, i) { // Helper function for createParticle()
+function particleWomb(x, y, i, ang) { // Helper function for createParticle()
 	trailParticles[i].style.top = y + "px";
 	trailParticles[i].style.left = x + "px";
 	var color = particleColors[Math.round(Math.random() * particleColors.length)];
+	trailParticles[i].x = Math.round(Math.cos(ang * Math.PI / 180) * trailConfig.particleSpeed);
+	trailParticles[i].y = Math.round(Math.sin(ang * Math.PI / 180) * trailConfig.particleSpeed);
 	trailParticles[i].style.backgroundColor = color;
 	trailParticles[i].lastUsed = Date.now();
 }
 trailConfig = { // Options
 	particleLife: 1000,
 	particleSpeed: 8,
-	particleQuantity: 150
+	particleAngle: function() {return 45}, // Can make animated in future
+	particleQuantity: 200
+}
+
+function randInt(min, max) {
+	var range = max - min;
+	return Math.round(Math.random() * range) - min;
 }
